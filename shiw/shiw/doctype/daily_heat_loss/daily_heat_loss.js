b116131b8b87df43a95cb695d9127c46d3859585
beetashoke <beetashoke.chakraborty@clapgrow.com>
2025-07-09 16:44:43 +0530

    muskan

---
+++ shiw/shiw/doctype/daily_heat_loss/daily_heat_loss.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, beetashoke.chakraborty@clapgrow.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Daily Heat Loss", {
+// 	refresh(frm) {
+
+// 	},
+// });
