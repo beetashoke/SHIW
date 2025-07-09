12f84c892dd87e8291fd0a83fef377ca76237204
beetashoke <beetashoke.chakraborty@clapgrow.com>
2025-07-09 12:06:27 +0530

    doctype

--- shiw/shiw/doctype/grade_master/grade_master.js
+++ shiw/shiw/doctype/grade_master/grade_master.js
@@ -1,8 +1,9 @@
 // Copyright (c) 2025, beetashoke.chakraborty@clapgrow.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Grade Master", {
-// 	refresh(frm) {
+frappe.ui.form.on("Grade Master", {
+    refresh(frm) {
+        console.log("hiiiii")
 
-// 	},
-// });
+    },
+});
